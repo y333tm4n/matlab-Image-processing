e27750733294873c89b4bc635e4394098a93463d
y333tm4n <pjccabigao12@plm.edu.ph>
2024-07-13 23:12:49 +0800

    again, changed the code

--- segmentation/segmentation.m
+++ segmentation/segmentation.m
@@ -1,66 +1,40 @@
-% % load the image 
-% img = imread("sunflower.jpg");
+% Global Image thresholding using Otsu's method
+% load image 
+img = imread('coins.png');
+
+% calculate threshold using graythresh
+level = graythresh(img);
+
+% convert into binary image using the computed threshold
+bw = imbinarize(img, level);
+
+% display the original image and the binary image
+figure; imshowpair(img, bw, 'montage'); title('Original Image (left) and Binary Image (right)');
 
 
-% % convert the image to grayscale
-% gray_img = rgb2gray(img);
+% Multi-level thresholding using Otsu's method
+% calculate single threshold using multithresh
+level = multithresh(img);
 
-% % using global thresholding
-% % compute the global threshold value
-% threshold = graythresh(gray_img);
+% Segment the image into two regions using the imquantize function, specifying the threshold level returned by the multithresh function. 
+seg_img = imquantize(img,level);
 
-% % apply the threshold to the image
-% bw_img = imbinarize(gray_img, threshold);
-
-% % display the original and the segmented image
-% figure(1);
-% imshowpair(img, bw_img, 'montage'); title('Original Image (left) vs. Segmented Image (right)');
-
-% % using adaptive thresholding
-% % apply the adaptive threshold to the image
-% at_value = adaptthresh(gray_img, 0.4, 'ForegroundPolarity', 'dark');
-
-% % convert the adaptive threshold to a binary image
-% at_img = imbinarize(gray_img, at_value);
-
-% % display the original and the segmented image
-% figure(2);
-% imshowpair(img, at_img, 'montage'); title('Original Image (left) vs. Segmented Image (right)');
-
-% % using Otsu's method
-% % calculate the histogram for the image
-% [counts, x] = imhist(gray_img, 16);
-
-% % calculate otsu's threshold
-% otsu_value = otsuthresh(counts);
-
-% % apply the threshold to the image
-% otsu_img = imbinarize(gray_img, otsu_value);
-
-% % display the original and the segmented image
-% figure(3);
-% imshowpair(img, otsu_img, 'montage'); title('Original Image (left) vs. Segmented Image (right)');
+% Display the original image and the segmented image
+figure; imshowpair(img,seg_img,'montage'); title('Original Image (left) and Segmented Image (right)');
 
 
-% ------------------------------------------------------------------------------------------------------------------------ %
+% Global histogram threshold using Otsu's method
+% Calculate a 16-bin histogram for the image
+[counts,x] = imhist(img,16);
+stem(x,counts)
 
-% Region based segmentation techniques
-% 1. connected components labeling: a technique that assigns a unique label to each connected component in an image.
+% Compute a global threshold using the histogram counts
+T = otsuthresh(counts);
 
-% Read and convert image to binary
-img2 = imread('coins.png');
-BW = imbinarize(im2gray(img2));
+% Create a binary image using the computed threshold and display the image
+bw = imbinarize(img,T);
+figure; imshow(bw); title('Binary Image');
 
-% Label connected components
-[L, num] = bwlabel(BW);
 
-% Display the original image and labeled image
-figure; imshow(BW); title('Original Image');
 
-% Display the labeled image
-figure; imshow(label2rgb(L, 'jet', 'k', 'shuffle'));
-
-% 2. Watershed segmentation: a technique that treats the image as a topographic surface and simulates flooding to segment the image.
-
-% Read and convert image to grayscale
 
