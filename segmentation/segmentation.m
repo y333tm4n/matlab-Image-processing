79111b035a411b4e2bbd6ee40fc8161faf3ec229
y333tm4n <pjccabigao12@plm.edu.ph>
2024-07-14 09:36:36 +0800

    added parameter modification and another region based segmentation technique

--- segmentation/segmentation.m
+++ segmentation/segmentation.m
@@ -1,39 +1,39 @@
 % Global Image thresholding using Otsu's method
-% load image 
-% img = imread('coins.png');
+load image 
+img = imread('coins.png');
 
-% % calculate threshold using graythresh
-% level = graythresh(img);
+% calculate threshold using graythresh
+level = graythresh(img);
 
-% % convert into binary image using the computed threshold
-% bw = imbinarize(img, level);
+% convert into binary image using the computed threshold
+bw = imbinarize(img, level);
 
-% % display the original image and the binary image
-% figure; imshowpair(img, bw, 'montage'); title('Original Image (left) and Binary Image (right)');
-
-
-% % Multi-level thresholding using Otsu's method
-% % calculate single threshold using multithresh
-% level = multithresh(img);
-
-% % Segment the image into two regions using the imquantize function, specifying the threshold level returned by the multithresh function. 
-% seg_img = imquantize(img,level);
-
-% % Display the original image and the segmented image
-% figure; imshowpair(img,seg_img,'montage'); title('Original Image (left) and Segmented Image (right)');
+% display the original image and the binary image
+figure(1); imshowpair(img, bw, 'montage'); title('Original Image (left) and Binary Image (right)');
 
 
-% % Global histogram threshold using Otsu's method
-% % Calculate a 16-bin histogram for the image
-% [counts,x] = imhist(img,16);
-% stem(x,counts)
+% Multi-level thresholding using Otsu's method
+% calculate single threshold using multithresh
+level = multithresh(img);
 
-% % Compute a global threshold using the histogram counts
-% T = otsuthresh(counts);
+% Segment the image into two regions using the imquantize function, specifying the threshold level returned by the multithresh function. 
+seg_img = imquantize(img,level);
 
-% % Create a binary image using the computed threshold and display the image
-% bw = imbinarize(img,T);
-% figure; imshow(bw); title('Binary Image');
+% Display the original image and the segmented image
+figure(2); imshowpair(img,seg_img,'montage'); title('Original Image (left) and Segmented Image (right)');
+
+
+% Global histogram threshold using Otsu's method
+% Calculate a 16-bin histogram for the image
+[counts,x] = imhist(img,16);
+stem(x,counts)
+
+% Compute a global threshold using the histogram counts
+T = otsuthresh(counts);
+
+% Create a binary image using the computed threshold and display the image
+bw = imbinarize(img,T);
+figure(3); imshow(bw); title('Binary Image');
 
 
 %----------------------------------------------------------------------------------------------------------------------------------------------------%
@@ -41,28 +41,58 @@
 % 2. Region-based segmentation
 
 % Using K means clustering
-% img2 = imread('paris.jpg');
+img2 = imread('paris.jpg');
 
-% % Convert the image to grayscale
-% bw_img2 = im2gray(img2);
-% imshow(bw_img2);
+% Convert the image to grayscale
+bw_img2 = im2gray(img2);
+imshow(bw_img2);
 
-% % Segment the image into three regions using k-means clustering
-% [L, centers] = imsegkmeans(bw_img2,3);
-% B = labeloverlay(bw_img2,L);
-% imshow(B);
-% title('Labled Image');
+% Segment the image into three regions using k-means clustering
+[L, centers] = imsegkmeans(bw_img2,3);
+B = labeloverlay(bw_img2,L);
+imshow(B); title('Labled Image');
 
-% Improvin kmeans with texture and spatial information
-RBG  = imread('paris.jpg');
-RGB  = imresize(RBG,0.5);
-imshow(RGB);
+
+% using connected-component labeling
+
+% convert the image into binary 
+bin_img2 = imbinarize(bw_img2);
+
+% Label the connected components
+[labeledImage, numberOfComponents] = bwlabel(bin_img2);
+
+% Display the number of connected components
+disp(['Number of connected components: ', num2str(numberOfComponents)]);
+
+% Assign a different color to each connected component
+coloredLabels = label2rgb(labeledImage, 'hsv', 'k', 'shuffle');
+
+% Display the labeled image
+imshow(coloredLabels);
+title('Labeled Image');
+
+
+%----------------------------------------------------------------------------------------------------------------------------------------------------%
+
+% Paramter Modifications 
+
+% adding noise to the image then segmenting it using otsu's method
+img_noise = imnoise(img,'salt & pepper',0.09);
+
+% calculate single threshold using multithresh
+level = multithresh(img_noise);
+
+% Segment the image into two regions using the imquantize function, specifying the threshold level returned by the multithresh function. 
+seg_img = imquantize(img_noise,level);
+
+% Display the original image and the segmented image
+figure(4); imshowpair(img_noise,seg_img,'montage'); title('Original Image (left) and Segmented Image with noise (right)');
+
 
 % Segment the image into two regions using k-means clustering
 L = imsegkmeans(RGB,2);
 B = labeloverlay(RGB,L);
-imshow(B);
-title('Labeled Image');
+figure(5); imshow(B); title('Labeled Image');
 
 % Create a set of 24 Gabor filters, covering 6 wavelengths and 4 orientations
 wavelength = 2.^(0:5) * 3;
@@ -94,8 +124,5 @@ L2 = imsegkmeans(featureSet,2,"NormalizeInput",true);
 C = labeloverlay(RGB,L2);
 imshow(C)
 title("Labeled Image with Additional Pixel Information")
-
-
-
 
 
